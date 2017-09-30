085c966777d47806a2f70a72d71dc691332ad4ee
Alastair <alastair.heggie@gmail.com>
2017-09-30 11:49:37 +0100

    Simple demmo
    Loop over files, create card object, get content etc. add to question/answer strings, stitch together, pandoc to epub

--- pubcards.py
+++ pubcards.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+
 '''
 generate a dict of topic:cards
 open two "files", one for questions, one for answers
@@ -9,11 +11,11 @@ for each topic:
 	choose a random card in the dict
 	assign that card an anchor pair from list of random numbers
 	assemble the answer card from the title, question content, link and anchor
-	assemble the solution card from the title, answer content, link and anchor
+	assemble the answer card from the title, answer content, link and anchor
 	write the answer card to the answer file
-	write the solution card to the solution file
+	write the answer card to the answer file
 
-join the answer file to the solution file
+join the answer file to the answer file
 
 convert to epuv
 
@@ -28,31 +30,71 @@ class card(object):
 		self.file = file
 		self.topics = None
 		self.question_content = None
-		self.solution_content = None
+		self.answer_content = None
 		self.title = None		
-	def parse(self):
+	def parse(self,p = None):
 		f = open(self.file,'r')
 		lines = f.readlines()
 		topics_idx = lines.index("%%%topics%%%\n")
 		question_idx = lines.index("%%%question%%%\n")
-		solution_idx = lines.index("%%%solution%%%\n")
+		answer_idx = lines.index("%%%answer%%%\n")
 		self.title = lines[0]
 		self.topics= [str.strip(x) for x in lines[topics_idx+1:question_idx] 
 		if x != "\n"]
 		self.question_content= str.strip("".join(
-			lines[question_idx+1:solution_idx]))
-		self.solution_content= str.strip("".join(
-			lines[solution_idx+1:len(lines)]))
-		print "title:", self.title
-		print "topics:", self.topics
-		print "Question:", self.question_content
-		print "Solution:", self.solution_content
+			lines[question_idx+1:answer_idx]))
+		self.answer_content= str.strip("".join(
+			lines[answer_idx+1:len(lines)]))
+		if p == True:
+			print("title:", self.title)
+			print("topics:", self.topics)
+			print("Question:", self.question_content)
+			print("answer:", self.answer_content)
+
+
+
+
 
 path = "cards"
+topics = set()
+questions , answers = str(), str()
+seedanchor =  99999
+
+
 for filename in os.listdir(path):
-	print filename
 	a=card(file = os.path.join(path,filename))
 	a.parse()
+	questionno = seedanchor+1
+	answerno = seedanchor+2
+	seedanchor = seedanchor+2
+
+	
+	topics.update(set(a.topics))
+	title ="#" + a.title
+	question_content=a.question_content+"\n\n"
+	answer_content=a.answer_content+"\n\n"
+	question_link = "[back to question](#question"+str(questionno)+")\n\n"
+	answer_link = "[see answer](#answer"+str(answerno)+")\n\n"
+	question_anchor = "#####question" + str(questionno) + "\n\n"
+	answer_anchor = "#####answer" + str(answerno) + "\n\n"
+
+	questions = (questions + title + question_content + 
+		answer_link + question_anchor)
+	answers = (answers + title + answer_content +
+		question_link + answer_anchor)
+
+header = "% My Flashcards\n% Alastair Heggie\n\n"
+book = header + questions + answers
+bookname = "flashcards"
+bf =  open(bookname+".md","w")
+bf.writelines(book)
+bf.close()
+
+
+args = ['pandoc', bookname+".md", '-o', bookname+".epub"]
+subprocess.Popen(args)
+
+
 ##method that assembles question card from title, content, link and anchor
 
 
